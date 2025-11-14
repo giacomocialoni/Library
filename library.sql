6fcd914e734a4e4881ac3614311ba8c9f3eb028e
Giacomo Cialoni <giacomo.cialoni@gmail.com>
2025-11-14 11:15:58 +0100

    Updated SQL script: new wishlist table, updated loan and purchase tables t manage stus

--- library.sql
+++ library.sql
@@ -16,6 +16,10 @@ CREATE TABLE `books` (
   KEY `fk_books_category` (`category`),
   CONSTRAINT `fk_books_category` FOREIGN KEY (`category`) REFERENCES `categories` (`category`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `categories` (
+  `category` varchar(50) NOT NULL,
+  PRIMARY KEY (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
@@ -24,15 +28,19 @@ CREATE TABLE `users` (
   `role` enum('admin','logged_user') NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `categories` (
-  `category` varchar(50) NOT NULL,
-  PRIMARY KEY (`category`)
+CREATE TABLE `posts` (
+  `user_fk` varchar(100) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text NOT NULL,
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_fk`,`post_date`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_fk`) REFERENCES `users` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `loans` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_email` varchar(100) NOT NULL,
   `book_id` int NOT NULL,
-  `status` enum('RESERVED','BORROWED','EXPIRED','RETURNED') NOT NULL DEFAULT 'RESERVED',
+  `status` enum('RESERVED','LOANED','EXPIRED','RETURNED') NOT NULL DEFAULT 'RESERVED',
   `reserved_date` date DEFAULT NULL,
   `loaned_date` date DEFAULT NULL,
   `returning_date` date DEFAULT NULL,
@@ -41,25 +49,26 @@ CREATE TABLE `loans` (
   KEY `book_id` (`book_id`),
   CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE,
   CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `purchases` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_email` varchar(100) NOT NULL,
   `book_id` int NOT NULL,
-  `purchase_date` date DEFAULT NULL,
   `status` enum('RESERVED','PURCHASED') NOT NULL DEFAULT 'RESERVED',
-  `approved_at` date DEFAULT NULL,
+  `status_date` date NOT NULL DEFAULT (curdate()),
   PRIMARY KEY (`id`),
   KEY `user_email` (`user_email`),
   KEY `book_id` (`book_id`),
   CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE,
   CONSTRAINT `purchases_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `posts` (
-  `user_fk` varchar(100) NOT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `content` text NOT NULL,
-  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_fk`,`post_date`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_fk`) REFERENCES `users` (`email`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `wishlist` (
+  `user_email` varchar(100) NOT NULL,
+  `book_id` int NOT NULL,
+  PRIMARY KEY (`user_email`,`book_id`),
+  UNIQUE KEY `unique_wishlist` (`user_email`,`book_id`),
+  KEY `user_email` (`user_email`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE,
+  CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
